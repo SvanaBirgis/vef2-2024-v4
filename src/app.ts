00f647f57621851dd5f007a2109943a9ce7b77b4
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2024-03-17 16:44:34 +0000

    fix: laga port

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors);
 app.use(router);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
